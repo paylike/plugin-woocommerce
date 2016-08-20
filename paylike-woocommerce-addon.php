5daea582432c19447a4e4fc8c44bb782e1aa1190
Nazrul <nazrulhassanmca@gmail.com>
2016-08-20 23:51:26 +0530

    Add files via upload

--- paylike-woocommerce-addon.php
+++ paylike-woocommerce-addon.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Plugin Name: Paylike Woo Addon
+ * Plugin Name: Paylike Payment for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/paylike-woocommerce-addon/
  * Description: This plugin adds a payment option in WooCommerce for customers to pay with their Credit Cards Via paylike.
- * Version: 1.0.0
+ * Version: 1.0
  * Author: Syed Nazrul Hassan
  * Author URI: https://nazrulhassan.wordpress.com/
  * Author Email : nazrulhassanmca@gmail.com
@@ -40,7 +40,7 @@ function paylike_init()
 			$this->method_title     = 'Paylike Cards Settings';
 			$this->init_form_fields();
 			$this->init_settings();
-			$this->supports                 = array('products');
+			$this->supports                 = array('products','refunds');
 			$this->title               	   	= $this->get_option( 'paylike_title' );
 			$this->paylike_description       = $this->get_option( 'paylike_description');
 
@@ -55,7 +55,7 @@ function paylike_init()
 			;
 			$this->paylike_cardtypes         = $this->get_option( 'paylike_cardtypes');
 			
-			$this->paylike_zerodecimalcurrency    = array("CLP","JPY","VND");
+			$this->paylike_zerocurrency    = array("CLP","JPY","VND");
 
 			if(!defined("PAYLIKE_TRANSACTION_MODE"))
 				{ define("PAYLIKE_TRANSACTION_MODE"  , ($this->paylike_authorize_only =='yes'? true : false)); }
@@ -72,6 +72,7 @@ function paylike_init()
 
 			add_action( 'woocommerce_receipt_' . $this->id, array( $this, 'receipt_page' ) );
 			add_action( 'woocommerce_api_' . strtolower( get_class( $this ) ), array( $this, 'return_handler' ) );
+			add_action( 'woocommerce_order_status_completed', array( $this, 'capture_payment' ) );
 
 			$this->publickey = 'yes'== $this->paylike_sandbox ? $this->paylike_testpublickey:$this->paylike_livepublickey;
 
@@ -93,7 +94,7 @@ function paylike_init()
 			{
 				?>
 				<h3><?php _e( 'Paylike addon for Woocommerce', 'woocommerce' ); ?></h3>
-				<p><?php  _e( 'Paylike is a company that provides a way for individuals and businesses to accept payments over the Internet.', 'woocommerce' ); ?></p>
+				<p><?php  _e( 'Paylike enables you to accept credit and debit cards on your WooCommerce platform. If you don\'t already have an account with Paylike, you can create it <a href="https://paylike.io" target="_blank">here</a>. Need help with the setup? Read our documentation  <a href="#" target="_blank">here</a>', 'woocommerce' ); ?></p>
 				<table class="form-table">
 					<?php $this->generate_settings_html(); ?>
 				</table>
@@ -116,16 +117,25 @@ function paylike_init()
 				'title' => __( 'Title', 'woocommerce' ),
 				'type' => 'text',
 				'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-				'default' => __( 'Paylike', 'woocommerce' ),
+				'default' => __( 'Credit card (Paylike)', 'woocommerce' ),
 				'desc_tip'      => true,
 				),
 			'paylike_description' => array(
 				'title' => __( 'Description', 'woocommerce' ),
 				'type' => 'textarea',
 				'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
-				'default' => __( 'Note: This form processes payments securely via &copy; <a href="https://paylike.io" target="_blank">Paylike</a>. Your card details <strong>never</strong> hit our server', 'woocommerce' ),
+				'default' => __( 'Secure payment via &copy; <a href="https://paylike.io" target="_blank">Paylike</a>', 'woocommerce' ),
 				'desc_tip'      => true,
 				),
+			'paylike_sandbox' => array(
+				'title'       => __( 'Test mode', 'woocommerce' ),
+				'type'        => 'checkbox',
+				'label'       => __( 'Enable Paylike sandbox (Live Mode if unchecked)', 'woocommerce' ),
+				'description' => __( 'If checked it\'s in sanbox mode and if unchecked it\'s in live mode', 'woocommerce' ),
+				'desc_tip'      => true,
+				'default'     => 'no',
+				),
+			
 			'paylike_testpublickey' => array(
 				'title' => __( 'Test public key', 'woocommerce' ),
 				'type' => 'text',
@@ -133,6 +143,15 @@ function paylike_init()
 				'default' => '',
 				'desc_tip'      => true,
 				'placeholder' => 'Paylike test api Key'
+				),
+
+			'paylike_testappkey' => array(
+				'title' => __( 'Test App Key', 'woocommerce' ),
+				'type' => 'text',
+				'description' => __( 'This is the App Key found in App Settings.', 'woocommerce' ),
+				'default' => '',
+				'desc_tip'      => true,
+				'placeholder' => 'Paylike test app key'
 				),
 
 			'paylike_livepublickey' => array(
@@ -144,14 +163,7 @@ function paylike_init()
 				'placeholder' => 'Paylike live api key'
 				),
 
-			'paylike_testappkey' => array(
-				'title' => __( 'Test App Key', 'woocommerce' ),
-				'type' => 'text',
-				'description' => __( 'This is the App Key found in App Settings.', 'woocommerce' ),
-				'default' => '',
-				'desc_tip'      => true,
-				'placeholder' => 'Paylike test app key'
-				),
+		
 			
 			'paylike_liveappkey' => array(
 				'title' => __( 'Live App Key', 'woocommerce' ),
@@ -162,14 +174,7 @@ function paylike_init()
 				'placeholder' => 'Paylike live app key'
 				),
 
-			'paylike_sandbox' => array(
-				'title'       => __( 'paylike Sandbox', 'woocommerce' ),
-				'type'        => 'checkbox',
-				'label'       => __( 'Enable Paylike sandbox (Live Mode if unchecked)', 'woocommerce' ),
-				'description' => __( 'If checked it\'s in sanbox mode and if unchecked it\'s in live mode', 'woocommerce' ),
-				'desc_tip'      => true,
-				'default'     => 'no',
-				),
+			
 			'paylike_authorize_only' => array(
 			 'title'       => __( 'Authorize Only', 'woocommerce' ),
 			 'type'        => 'checkbox',
@@ -187,9 +192,9 @@ function paylike_init()
 				'desc_tip' => __( 'Select the card types to accept.', 'woocommerce' ),
 				'options'  => array(
 					'mastercard'       => 'MasterCard',
-					'maestro'       => 'Maestro',
-					'visaelectron'       => 'Visa Electron',
+					'maestro'          => 'Maestro',
 					'visa'             => 'Visa',
+					'visaelectron'     => 'Visa Electron'
 					),
 				'default' => array( 'mastercard', 'maestro', 'visa', 'visaelectron' ),
 				),
@@ -290,48 +295,34 @@ function paylike_init()
 			);
 		}
 
-		
-		public function get_amount( $total,$currency =null ) {
-
-			$curcode  = $currency != '' ? $currency : get_woocommerce_currency() ; 
-			
-			if(in_array( $curcode ,$this->paylike_zerodecimalcurrency ))
-            {
-                $amount          = number_format($total,0,".","") ;
-            }
-            else
-            {
-                $amount          = $total * 100 ;
-            }
-
-			return $amount;
-		}
+	
 
 
 		public function receipt_page( $order_id ) {
 
 			$order = wc_get_order( $order_id );
 
-			$amount = $this->get_amount( $order->order_total,$order->get_order_currency() ) ;
-
 			echo '<p>' . __( 'Thank you for your order, please click below to pay and complete your order.', 'woocommerce' ) . '</p>';
 
 			?>
 			<button onclick="pay();">Pay Now</button>
-			<script src="https://sdk.paylike.io/2.js"></script>
+			<script src="https://sdk.paylike.io/3.js"></script>
 			<script>
 			var paylike = Paylike('<?php echo $this->publickey ;?>');
 
 			function pay(){
 
 			paylike.popup({
+				locale          : '<?php echo get_locale() ?>',
 				title           : '<?php echo bloginfo('name'); ?>',
+    			description     : '<?php echo '#Order '.$order->get_order_number() ?>',  
 				currency        : '<?php echo get_woocommerce_currency() ?>',
-				amount          :  <?php echo $amount; ?>,
+				amount          :  <?php echo $this->paylike_order_total($order) ; ?>,
 				custom: {
-					orderNo     : '<?php echo $order->get_order_number() ?>',
+					OrderNo     : '<?php echo $order->get_order_number() ?>',
 					email       : '<?php echo $order->billing_email ?>',
-					name        :  '<?php echo $order->billing_first_name.' '.$order->billing_last_name ?>',
+					name        : '<?php echo $order->billing_first_name.' '.$order->billing_last_name ?>',
+					country     : '<?php echo $order->shipping_country ; ?>',
 					totalTax    : '<?php echo $order->get_total_tax()?>',
 					totalShipping : '<?php echo $order->get_total_shipping()?>',
 					customerIP	: '<?php echo $this->get_client_ip() ?>',
@@ -362,20 +353,31 @@ function paylike_init()
 try 
 {
 
-	if (  isset( $_REQUEST['reference'] )  && isset( $_REQUEST['signature'] ) 
- 			&& isset( $_REQUEST['transactionid'] ) && isset($_REQUEST['amount']) ) 
+// Array
+// (
+//     [reference] => 28
+//     [amount] => 8.17
+//     [signature] => 0F4FCAA36A80E42501EB8ABC067CC561
+//     [transactionid] => 57b782674466127803da1523
+// )
+
+
+	if (  isset( $_REQUEST['reference'] )  && isset($_REQUEST['amount']) && isset( $_REQUEST['signature'] ) && isset( $_REQUEST['transactionid'] )  ) 
 	{
 
-		$signature = strtoupper( md5( $_REQUEST['amount'].$_REQUEST['reference'].$this->publickey ) );
-		$order_id  = absint( $_REQUEST['reference'] );
+		$signature = strtoupper( md5( sanitize_text_field($_REQUEST['amount']).sanitize_text_field($_REQUEST['reference']).$this->publickey ) );
+		$order_id  = absint( sanitize_text_field($_REQUEST['reference'] ) );
 		$order     = wc_get_order( $order_id );
+
+		 
 				
-	if ( $signature === $_REQUEST['signature'] ) 
+	if ( $signature === sanitize_text_field($_REQUEST['signature']) ) 
 	{
-		$transactionid = $_REQUEST['transactionid'];
+		$transactionid = sanitize_text_field($_REQUEST['transactionid']);
 
 		if(true == PAYLIKE_TRANSACTION_MODE)
 		{
+			//Authorize only Transaction
 			$verifyurl = 'https://api.paylike.io/transactions/'.$transactionid ; 
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL,$verifyurl);
@@ -386,21 +388,25 @@ try
 			$result = curl_exec($ch);
 			curl_close($ch);
 			$verifyres = json_decode($result,true) ; 
- 		
+ 			
 
-			 if( $verifyres['transaction']['currency'] == $order->get_order_currency() &&  $verifyres['transaction']['custom']['OrderNo'] == $order->get_order_number() && $verifyres['transaction']['amount'] == $this->get_amount($order->order_total,$order->get_order_currency()) )
+			 if( $verifyres['transaction']['currency'] == $order->get_order_currency() &&  $verifyres['transaction']['custom']['OrderNo'] == $order->get_order_number() && $verifyres['transaction']['amount'] == $this->paylike_order_total($order) )
 			 {
-			 	$order->payment_complete($transactionid);
 
+			 	$order->payment_complete($transactionid);
 			 	$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Payment Amount : '.$verifyres['transaction']['amount'].', Charge authorized at : '.$verifyres['transaction']['created'] ,'woocommerce'));
+			 	add_post_meta( $order->id, '_paylike_charge_status', 'charge_auth_only');
 
 				WC()->cart->empty_cart();
 			 }
+
+
  	    }
  	    else
  	    {
+ 	    	//Authorize and Capture only Transaction
  	    $data = array(
-    	'amount'     => $this->get_amount($order->order_total,$order->get_order_currency()) ,
+    	'amount'     => $this->paylike_order_total($order) ,
     	'currency'   => $order->get_order_currency()  ) ;
  	    	
 
@@ -422,7 +428,7 @@ try
 
 				$order->payment_complete($transactionid);
 
-			 	$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Authorized Amount : '.$verifyres['transaction']['amount'].'Captured Amount '.$verifyres['transaction']['capturedAmount'].', Charge authorized at : '.$verifyres['transaction']['created'] ,'woocommerce'));
+			 	$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Authorized Amount : '.$verifyres['transaction']['amount'].', Captured Amount '.$verifyres['transaction']['capturedAmount'].', Charge authorized at : '.$verifyres['transaction']['created'] ,'woocommerce'));
 
 				WC()->cart->empty_cart();
 			}
@@ -459,36 +465,214 @@ try
 
 
 
+
+
+		public function capture_payment($order_id){
+
+			$order = new WC_Order($order_id);
+
+			try
+	       	{
+				$transactionid     = get_post_meta( $order_id, '_transaction_id', true );
+				$chargestatus = get_post_meta( $order_id, '_paylike_charge_status', true );
+				if($chargestatus == 'charge_auth_only')
+				{
+		       	 	
+
+					
+					$data = array(
+									'amount'     => $this->paylike_capture_total($order) ,
+									'currency'   => $order->get_order_currency()  
+								 ) ;
+
+
+					$verifyurl = 'https://api.paylike.io/transactions/'.$transactionid.'/captures' ; 
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL,$verifyurl);
+					curl_setopt($ch, CURLOPT_HEADER, false);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+					curl_setopt($ch, CURLOPT_USERPWD, ":" .$this->appkey);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+					$result = curl_exec($ch);
+					$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+					curl_close($ch);
+					$verifyres = json_decode($result,true) ; 
+
+					if($httpcode >= 200 || $httpcode <= 299 )
+					{
+
+					
+
+					$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Authorized Amount : '.$verifyres['transaction']['amount'].', Captured Amount '.$verifyres['transaction']['capturedAmount'].', Charge authorized at : '.$verifyres['transaction']['created'] ,'woocommerce'));
+
+				
+					}
+					else
+					{
+					foreach ($verifyres as $value) 
+					{
+						$error[] = $value['message'];
+					}
+
+					$errorstring =  implode(" ",$error);
+
+					$order->add_order_note(__( 'Error = '.$errorstring ,'woocommerce'));
+					}
+
+
+				}
+
+			}
+			catch(Exception $e)
+			{
+
+	        	update_post_meta( $order_id, '_stripe_charge_status', 'charge_auth_expired');
+	      	$wc_order->add_order_note(__( $captureresponse->status.' '.$e->getMessage(),'woocommerce'));
+	        }
+
+    	}
+
+    	/*Calculate order total*/
+    	private function paylike_order_total($order)
+		{
+			$grand_total 	= $order->order_total;
+			$currency = '' != $order->get_order_currency() ? $order->get_order_currency() : get_woocommerce_currency() ;
+
+			if(in_array($currency ,$this->paylike_zerocurrency ))
+			{
+				$amount 	     = number_format($grand_total,0,".","") ;
+			}
+			else
+			{
+				$amount 	     = $grand_total * 100 ;
+			}
+
+			return $amount;
+		}
+		/*Calculate order total*/
+
+    	/*Calculate refund total */
+		private function paylike_refund_total($order, $ramount)
+        {
+            $currency = '' != $order->get_order_currency() ? $order->get_order_currency() : get_woocommerce_currency() ;
+
+            if(in_array($currency ,$this->paylike_zerocurrency ))
+            {
+                    $ramount      = number_format($ramount,0,".","") ;
+            }
+            else
+            {
+                    $ramount      = $ramount * 100 ;
+            }
+
+            return $ramount;
+        }
+        /*Calculate refund total */
+
+
+        /*Calculate capture total */
+		private function paylike_capture_total($order)
+        {
+            $currency = '' != $order->get_order_currency() ? $order->get_order_currency() : get_woocommerce_currency() ;
+
+            if(in_array($currency ,$this->paylike_zerocurrency ))
+            {
+                    $camount      = number_format($order->get_remaining_refund_amount(),0,".","") ;
+            }
+            else
+            {
+                    $camount      = $order->get_remaining_refund_amount() * 100 ;
+            }
+
+            return $camount;
+        }
+        /*Calculate capture total */
+
+
+
 		/*process refund function*/
 		public function process_refund($order_id, $amount = NULL, $reason = '' ) {
 
 
 			if($amount > 0 )
 			{
-				$CHARGE_ID 	= get_post_meta( $order_id , '_transaction_id', true );
-				$charge 		= paylike_Charge::retrieve($CHARGE_ID);
-				$refund 		= $charge->refunds->create(
-					array(
-						'amount' 		=> $amount * 100,
-						'metadata'	=> array('Order #' 		=> $order_id,
-							'Refund reason' => $reason
-							),
-						)
-					);
-				if($refund)
-				{
+				$transactionid 	= get_post_meta( $order_id , '_transaction_id', true );
+				$order          = new WC_Order( $order_id );
+				
 
-					$rtimestamp  = date('Y-m-d H:i:s A e', $refund->created);
-					$refundid    = $refund->id;
-					$wc_order    = new WC_Order( $order_id );
-					$wc_order->add_order_note( __( 'paylike Refund completed at. '.$rtimestamp.' with Refund ID = '.$refundid , 'woocommerce' ) );
-					return true;
+				if(true == PAYLIKE_TRANSACTION_MODE)
+				{	
+
+					$data = array(
+										'amount'     => $this->paylike_refund_total($order,$amount), 
+									 ) ;
+
+
+						$verifyurl = 'https://api.paylike.io/transactions/'.$transactionid.'/voids' ; 
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL,$verifyurl);
+						curl_setopt($ch, CURLOPT_HEADER, false);
+						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+						curl_setopt($ch, CURLOPT_USERPWD, ":" .$this->appkey);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+						$result = curl_exec($ch);
+						$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+						curl_close($ch);
+						$verifyres = json_decode($result,true) ; 
+
+					
+					if($verifyres)
+					{
+
+
+						$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Voided Amount : '.$verifyres['transaction']['voidedAmount'].', Pending Amount : '.$verifyres['transaction']['pendingAmount'].', Void created at : '.$verifyres['transaction']['created']   ,'woocommerce'));
+
+						return true;
+					}
+					else
+					{
+						return false;
+					}
 				}
 				else
-				{
-					return false;
-				}
+				{			
+					$data = array(
+										'amount'     => $this->paylike_refund_total($order,$amount), 
+										'descriptor'   =>$reason 
+									 ) ;
 
+
+						$verifyurl = 'https://api.paylike.io/transactions/'.$transactionid.'/refunds' ; 
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL,$verifyurl);
+						curl_setopt($ch, CURLOPT_HEADER, false);
+						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+						curl_setopt($ch, CURLOPT_USERPWD, ":" .$this->appkey);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+						$result = curl_exec($ch);
+						$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+						curl_close($ch);
+						$verifyres = json_decode($result,true) ; 
+
+
+						
+					
+					if($verifyres)
+					{
+
+
+						$order->add_order_note(__( 'Trx ID = '.$verifyres['transaction']['id'].' Refund Amount : '.$verifyres['transaction']['refundedAmount'].', Refund created at : '. date('Y-m-d H:i:s A e', $verifyres['transaction']['created'] )  ,'woocommerce'));
+
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+			    }
 
 			}
 			else
@@ -508,3 +692,19 @@ try
 
 }
 add_action( 'plugins_loaded', 'paylike_init' );
+
+
+
+/*Plugin Settings Link*/
+function paylike_woocommerce_settings_link( $links ) {
+    $settings_link = '<a href="admin.php?page=wc-settings&tab=checkout&section=paylike">' . __( 'Settings' ) . '</a>';
+    $doc_link = '<a  target="_blank" href="http://www.google.com">' . __( 'Documentation' ) . '</a>';
+    array_push( $links, $settings_link ,$doc_link);
+    
+  	return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'paylike_woocommerce_settings_link' );
+
+
+/*Plugin Settings Link*/
