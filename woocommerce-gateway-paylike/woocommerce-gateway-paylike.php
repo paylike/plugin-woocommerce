5e1fbd71a68a16b96f208cf5a9cedc6701b77333
Ionut Calara <ionut.calara@gmail.com>
2020-02-26 14:34:33 +0200

    Updated subscription

--- woocommerce-gateway-paylike/woocommerce-gateway-paylike.php
+++ woocommerce-gateway-paylike/woocommerce-gateway-paylike.php
@@ -5,11 +5,11 @@
  * Description: Allow customers to pay with credit cards via the Paylike gateway in your WooCommerce store.
  * Author: Derikon Development
  * Author URI: https://derikon.com/
- * Version: 1.8.0
+ * Version: 1.8.1
  * Text Domain: woocommerce-gateway-paylike
  * Domain Path: /languages
  * WC requires at least: 3.0
- * WC tested up to: 3.8.1
+ * WC tested up to: 3.9.2
  *
  * Copyright (c) 2016 Derikon Development
  *
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'WC_PAYLIKE_VERSION', '1.8.0' );
+define( 'WC_PAYLIKE_VERSION', '1.8.1' );
 define( 'WC_PAYLIKE_MIN_PHP_VER', '5.3.0' );
 define( 'WC_PAYLIKE_MIN_WC_VER', '2.5.0' );
 define( 'WC_PAYLIKE_MAIN_FILE', __FILE__ );
