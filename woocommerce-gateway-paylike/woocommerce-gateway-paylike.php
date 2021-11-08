d8c5eaf17fa663bcd238cb5ba1fbabe27d579f62
Ionut Calara <ionut.calara@gmail.com>
2021-11-08 10:39:56 +0200

    Add order currency on pay after page

--- woocommerce-gateway-paylike/woocommerce-gateway-paylike.php
+++ woocommerce-gateway-paylike/woocommerce-gateway-paylike.php
@@ -5,11 +5,11 @@
  * Description: Allow customers to pay with credit cards via Paylike in your WooCommerce store.
  * Author: Derikon Development
  * Author URI: https://derikon.com/
- * Version: 3.1.0
+ * Version: 3.1.1
  * Text Domain: woocommerce-gateway-paylike
  * Domain Path: /languages
  * WC requires at least: 3.0
- * WC tested up to: 4.9.2
+ * WC tested up to: 5.8.1
  *
  * Copyright (c) 2016 Derikon Development
  *
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'WC_PAYLIKE_VERSION', '3.0.0' );
+define( 'WC_PAYLIKE_VERSION', '3.1.1' );
 define( 'WC_PAYLIKE_MIN_PHP_VER', '5.3.0' );
 define( 'WC_PAYLIKE_MIN_WC_VER', '2.5.0' );
 define( 'WC_PAYLIKE_CURRENT_SDK', 10 );
